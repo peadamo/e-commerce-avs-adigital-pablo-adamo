4e472a10c3c26abe92cb96119bd8b337a54b9fc8
Your Namepablo <peadamo@gmail.com>
2022-03-09 00:15:37 -0300

    correccion para netlify

--- src/components/item-detail/ItemDetail.js
+++ src/components/item-detail/ItemDetail.js
@@ -1,45 +1,16 @@
-import { useState, useEffect } from "react";
 import ItemCounter from "../item-cunter/ItemCounter";
 import { Link } from "react-router-dom";
-import CartConteiner from "../cart/CartConteiner";
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import { Box, Skeleton } from "@mui/material";
+import { Box } from "@mui/material";
 import "./ItemDetail.css";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
 import { MultilineText } from "./MultilineText";
 const Item = styled(Paper)(() => ({}));
 
 const ItemDetail = ({ showProduct }) => {
-  const [totalToAdd, setTotalToAdd] = useState(null);
-  const [cardFoot, setCardFoot] = useState(null);
-  const [isImgLoad, setIsImgLoad] = useState(false);
 
-  useEffect(() => {
-    if (totalToAdd > 0) {
-      setCardFoot(
-        <>
-          <h5>Agregaste {totalToAdd} unidades al carrito</h5>
-          <Link to="/cart">
-            <button>Terminar mi compra</button>
-          </Link>
-        </>
-      );
-    } else {
-      setCardFoot(
-        <ItemCounter
-          stock={showProduct.stock}
-          setTotalToAdd={setTotalToAdd}
-          item={{ ...showProduct }}
-        />
-      );
-    }
-  }, [totalToAdd, showProduct]);
-console.log(showProduct.fullDescription)
+
   return (
     <>
       <Box sx={{ flexGrow: 1, display: "inline-flex" }}>
@@ -86,7 +57,6 @@ console.log(showProduct.fullDescription)
           <h6>Stock disponible {showProduct.stock} u.</h6>
           <ItemCounter
             stock={showProduct.stock}
-            setTotalToAdd={setTotalToAdd}
             item={{ ...showProduct }}
           />
           <Link to="/cart">
